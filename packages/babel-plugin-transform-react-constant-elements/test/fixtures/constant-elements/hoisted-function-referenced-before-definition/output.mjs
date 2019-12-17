5c679edf6046de8a449d2bff4ad2d6b7bfd1ddd5
Gord Pearson <gord.pearson@shopify.com>
2019-12-17 09:16:51 -0500

    Test showing hoisted function reference bug

---
+++ packages/babel-plugin-transform-react-constant-elements/test/fixtures/constant-elements/hoisted-function-referenced-before-definition/output.mjs
@@ -0,0 +1,21 @@
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function Foo() {
+  return _ref;
+}
+
+var _ref2 =
+/*#__PURE__*/
+_react["default"].createElement("div", null);
+
+function Bar() {
+  return _ref2;
+}
+
+var _ref =
+/*#__PURE__*/
+_react["default"].createElement(Bar, null);
